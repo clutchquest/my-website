c8bedfdc32d3813a05d9e73bf7a32438fc422aea
clutchquest <closeinquires@yahoo.com>
2025-07-03 12:41:00 -0700

    Changed the design of website, may changhe in the near future

--- src/Article.js
+++ src/Article.js
@@ -1,8 +1,14 @@
 import React from 'react';
 function Article(){
     return(
-       <article class= 'newsflash'>
-        <h1>Welcome</h1>
+       <article class= 'outer'>
+         <div class ='inner'>
+           <img src ="funny-guy-saying-ok-isolated-white-background-73044470-3616619068.jpg"></img>
+                <h1>Welcome to my Website!</h1>
+                 <p>Hey there, welcome to my website.
+                  Hope you are intrigued into the many things
+                  i have to offer!</p>
+            </div>
        </article>
     )
 }
